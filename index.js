e554cc06c85c60bdf606d371dfa8c18fbef9b3b1
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-07-16 22:11:28 -0500

    Finished on Functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const generateBackground = document.querySelector('#generate-background');
 const pictureDiv = document.querySelector('#picture');
-
+const picName = document.querySelector('h1');
 
 const randNum = (num) => {
     return Math.floor((Math.random() * num));
@@ -8,7 +8,13 @@ const randNum = (num) => {
 const displayPicture = () => {
     axios.get('http://localhost:4000/api/background').then((res) => {
         console.log(res.data);
+        if(document.querySelector('img')){
+            document.querySelector('img').remove();
+        }
         let index = randNum(res.data.length);
+        let newImg = document.createElement('img');
+        picName.textContent = res.data[index].name;
+        pictureDiv.appendChild(newImg).src = res.data[index].url;
         console.log(res.data[index].name)
         console.log(res.data[index].url)
         
