6a7bd7ca4381231a56be0682e329d79052010427
mroseweir <roseweirmichael@gmail.com>
2021-07-19 21:47:51 -0500

    adds styling and copy url button

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const generateBackground = document.querySelector('#generate-background');
+        const copyBackground = document.querySelector('#copy-button');      
 const pictureDiv = document.querySelector('#picture');
 const picName = document.querySelector('h1');
+let imgUrl = '';
 
 const randNum = (num) => {
     return Math.floor((Math.random() * num));
 }
+
+
 const displayPicture = () => {
     axios.get('http://localhost:4000/api/background').then((res) => {
         console.log(res.data);
@@ -13,12 +17,27 @@ const displayPicture = () => {
         }
         let index = randNum(res.data.length);
         let newImg = document.createElement('img');
+        imgUrl = res.data[index].url;  
         picName.textContent = res.data[index].name;
         pictureDiv.appendChild(newImg).src = res.data[index].url;
         console.log(res.data[index].name)
         console.log(res.data[index].url)
-        
     })
-}
+};
+
+
+const copyUrl = () => {
+    navigator.clipboard.writeText(imgUrl).then(() => {
+        let notification = document.getElementById('notification')
+        // notification.innerHTML = 'URL Copied Successfully!';
+        notification.style.visibility = 'visible'
+        // document.querySelector('#button-container').appendChild(notification)
+        setTimeout(() => {
+            notification.style.visibility = 'hidden';
+        }, 2000);
+    });
+    console.log(imgUrl);
+};;
 
 generateBackground.addEventListener('click', displayPicture);
+copyBackground.addEventListener('click', copyUrl);
