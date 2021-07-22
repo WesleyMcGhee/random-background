d7d781149fcf9be114c623dc59803e13367c549f
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-07-21 19:30:08 -0500

    fixed

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const port = process.env.PORT || 4000;
 const path = require('path')
 
-
+app.use(express.static("public"))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'../public'));
 })
